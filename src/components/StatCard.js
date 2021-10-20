d2b867455e1125806a988429a364627f915cf5bd
Zboru <zboru99@gmail.com>
2021-10-20 15:47:10 +0200

    early implementation of dark mode

--- src/components/StatCard.js
+++ src/components/StatCard.js
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
 const StatCard = ({children, className}) => {
-    return <div className={clsx(className, "p-5 border")}>{children}</div>
+    return <div className={clsx(className, "p-5 border dark:text-white dark:border-gray-500")}>{children}</div>
 }
 export default StatCard
